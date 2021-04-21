facd74215434555a93cc9f79d3a3d79e587ada8e
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-04-21 08:03:06 -0400

    kiev: Switch to prebuilt audio hal :/
    Change-Id: I6f8f6a7469c4d7ae09c1bd5abd7d135aefc12a5b

--- device.mk
+++ device.mk
@@ -34,10 +34,6 @@ PRODUCT_PACKAGES += \
 	NoCutoutOverlay
 
 # Audio
-PRODUCT_PACKAGES += \
-    audio.primary.lito \
-    libqcompostprocbundle
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
